ead30274e735137a871d0bcd497958166c430a4e
astarunova@talend.com <Door123!Bell>
2019-03-06 16:47:35 +0200

    Added config.properties and their reading

--- src/test/java/NytApiTest.java
+++ src/test/java/NytApiTest.java
@@ -1,23 +1,36 @@
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import io.restassured.response.Response;
 import io.restassured.matcher.RestAssuredMatchers.*;
 import org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
+import java.io.*;
+import java.util.Properties;
+
 public class NytApiTest {
 
-    @BeforeTest
+public static Properties pullingProperties() {
+    Properties prop = new Properties();
+    try {
+        prop.load(NytApiTest.class.getResourceAsStream("config.properties"));
+    } catch (Exception e) {
+        System.err.println("File does not exist");
+    }
+    return prop;
+}
+    @BeforeClass
     public void setup() {
-        baseURI = "https://api.nytimes.com";
+        baseURI = pullingProperties().getProperty("base.url");
     }
     @Test
     public void getMostEmailedArticles() {
+        System.out.println(baseURI);
         given().log().all().
-                param("api-key", "6Q1FfFmOGfCibBqgCPBAx7AF27ZrGEl4").
+                param("api-key", pullingProperties().getProperty("api.key")).
         when().
                 get("/svc/mostpopular/v2/emailed/{period}.json", 1).
                 then().log().body().statusCode(200);
